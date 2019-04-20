ab50a0ef30e6e3413de8d432c86112b8e86a6d41
Daniel Rose-Levine <droselevine18@simons-rock.edu>
2019-04-19 23:37:22 -0400

    did it

--- dictionary.c
+++ dictionary.c
@@ -4,10 +4,17 @@
 //forms hash value for string s
 //this produces a starting value in the dictionary array
 unsigned hash(const char *s) {
-	unsigned hashval;
-	//TODO - implement polynomial hashing of string s
-	
-	return hashval ;
+	unsigned hashval = 0;
+	int N = strlen(s);
+	for(int i = 0; i < N; i++) {
+		int num = s[i];
+		int A = 1;
+		for(int j = 0; j < N - 1 - i; j++) {
+			A *= 31;
+		}
+		hashval += num * A;
+	}
+	return hashval;
 }
 
 //Performs search for a key in the hashtable.
--- test_dictionary.c
+++ test_dictionary.c
@@ -3,6 +3,12 @@
 #include <ctype.h>
 #include "dictionary.h"
 
+int comp(const void *a, const void *b) {
+	char hi = *(const char *)a;
+	char ih = *(const char *)b;
+	return hi - ih;
+}
+
 void print_list(VNode *head){
 	VNode *cur = head;
 	while(cur){
@@ -34,6 +40,7 @@ int main (int argc, char ** argv) {
 	
 	set(dictionary, DEFAULT_HASH_SIZE, "bore", "robe");
 	result = get (dictionary, DEFAULT_HASH_SIZE, "bore");
+
 	if (result != NULL) {
 		printf("Anagrams for 'bore':\n");
 		print_list (result->values);
@@ -42,7 +49,44 @@ int main (int argc, char ** argv) {
 		printf ("'bore' not found\n");
 	
 	free_dictionary(dictionary, DEFAULT_HASH_SIZE);
-
 	
+	char **words = malloc(1000*sizeof(char*));
+	for(int i = 0; i < 1000; i++) {
+		words[i] = malloc(100*sizeof(char));
+	}
+    FILE *f = fopen("words.txt", "r");
+	int i = 0;
+	while(fgets(words[i], 100, f) != NULL) {
+		words[i][strlen(words[i]) - 2] = '\0';
+		i++;
+	}
+	fclose(f);
+	for(int i = 0; i < 1000; i++) {
+		char sort[strlen(words[i])];
+		for(int j = 0; j < strlen(words[i]); j++) {
+			sort[j] = words[i][j];
+		}
+		sort[strlen(words[i])] = '\0';
+		qsort(sort, strlen(sort), sizeof(char), comp);
+		set(dictionary, DEFAULT_HASH_SIZE, sort, words[i]);
+	}
+	printf("\n");
+	int count = 0;
+	for(int i = 0; i < DEFAULT_HASH_SIZE; i++) {
+		if(dictionary[i] == NULL)
+		continue;
+		DNode* node = dictionary[i];
+		while(node != NULL) {
+			if(node->values->next == NULL) {
+				node = node->next;
+				continue;
+			}
+			count++;
+			print_list(node->values);
+			node = node->next;
+		}
+	}
+	printf("%d anagram lists", count);
 	return 0;
 }
+
